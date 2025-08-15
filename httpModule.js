2d2d1396fcc78af3b48add46c42e154ab339b99f
Anant Kumar <adwayaanant@gmail.com>
2025-08-15 22:38:35 +0530

    practice diffrent router

--- httpModule.js
+++ httpModule.js
@@ -1,7 +1,15 @@
 const http = require('http');
 // importing the http module    
 const server = http.createServer((req, res) => {
-    res.write("Hello, World from server !");
+    if (req.url === '/') {
+        
+        res.write("Welcome to the home page!");
+    }else if (req.url === '/about') {
+        res.write("Welcome to the about page!");
+    } else {
+        res.write("Page not found!");
+    }
+
     res.end();
 });
 server.listen(3000, () => {
